46add3722aa8dcd27f3418c1c793445907d73585
Oluwatobi Ojelabi <tobi.oje@icloud.com>
2023-09-15 23:15:38 +0100

    Initial commit

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,5 @@
+const SideBar = () => {
+  return <h1>Side Bar Content</h1>;
+};
+
+export default SideBar;
---
+++ src/components/StartPage/StartPage.tsx
@@ -0,0 +1,9 @@
+const StartPage = () => {
+  return (
+    <>
+      <h1>Start Page</h1>
+    </>
+  );
+};
+
+export default StartPage;
